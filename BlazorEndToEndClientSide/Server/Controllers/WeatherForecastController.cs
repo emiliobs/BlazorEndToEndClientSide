d1aa2b6e571b83678eb9ad58451d4d2bdc2dceb6
Emilio <barrera_emilio@yahoo.es>
2020-03-05 18:28:36 +0000

    Edit forecast already

--- BlazorEndToEndClientSide/Server/Controllers/WeatherForecastController.cs
+++ BlazorEndToEndClientSide/Server/Controllers/WeatherForecastController.cs
@@ -1,12 +1,10 @@
-﻿using BlazorEndToEndClientSide.Shared;
-using System;
+﻿using BlazorEndToEndClientSide.EndToEnd.Data.EndTEnd;
+using BlazorEndToEndClientSide.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using BlazorEndToEndClientSide.EndToEnd.Data.EndTEnd;
-using Microsoft.EntityFrameworkCore;
 
 namespace BlazorEndToEndClientSide.Server.Controllers
 {
@@ -20,14 +18,14 @@ namespace BlazorEndToEndClientSide.Server.Controllers
         //Inject the EndToEndContext using dependency injection
         public WeatherForecastController(EndToEndContext context)
         {
-            this._context = context;
+            _context = context;
         }
 
         [HttpGet]
         [Route("api/WeatherForecast/GetAsync")]
         public async Task<List<WeatherForecastDTO>> GetAsync()
         {
-            var result = await _context.WeatherForecast.ToListAsync();
+            List<WeatherForecast> result = await _context.WeatherForecast.ToListAsync();
 
             return result.Select(x => new WeatherForecastDTO
             {
@@ -64,20 +62,37 @@ namespace BlazorEndToEndClientSide.Server.Controllers
 
         [HttpPost]
         [Route("/api/WatherForecast/Post")]
-        public void Post([FromBody] WeatherForecastDTO  model)
+        public void Post([FromBody] WeatherForecastDTO model)
         {
-            var weatheForecast = new WeatherForecast 
+            WeatherForecast weatheForecast = new WeatherForecast
             {
                 Id = model.Id,
                 UserName = model.UserName,
                 Date = model.Date,
                 Summary = model.Summary,
-                TemperatureC =model.TemperatureC,
+                TemperatureC = model.TemperatureC,
                 TemperatureF = model.TemperatureF,
             };
 
             _context.Add(weatheForecast);
             _context.SaveChanges();
+        }
+
+        [HttpPut]
+        [Route("api/WatherForecast/Put")]
+        public void Put([FromBody] WeatherForecastDTO model)
+        {
+            WeatherForecast existingForecast = _context.WeatherForecast.FirstOrDefault(w => w.Id == model.Id);
+
+            if (existingForecast != null)
+            {
+                existingForecast.Date = model.Date;
+                existingForecast.Summary = model.Summary;
+                existingForecast.TemperatureC = model.TemperatureC;
+                existingForecast.TemperatureF = model.TemperatureF;
+                existingForecast.UserName = model.UserName;
+                _context.SaveChanges();
+            }
         }
     }
 }
